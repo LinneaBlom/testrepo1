d6aefdae756244a528d9f8c472e15541d83d0506
linnea-blom <linnea.blom@king.com>
2021-07-23 14:41:59 +0200

    merge.sh

--- merge.sh
+++ merge.sh
@@ -2,6 +2,6 @@
 git checkout master
 git checkout -b cleanup/new4
 git merge develop || true
-git checkout --theirs -- jenkins.propertis
+git checkout --theirs -- jenkins.properties
 git commit -a -m "Update branch with latest develop " || true
 git push --set-upstream origin cleanup/new4
