ae923d864240923a9c06d14c80a8a969daa514a6
linnea-blom <linnea.blom@king.com>
2021-07-23 14:35:05 +0200

    merge.sh

--- merge.sh
+++ merge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 git checkout develop
-git checkout -b cleanup/new
+git checkout -b cleanup/new2
 git merge develop || true
 git commit -a -m "Update branch with latest develop "
-git push --set-upstream origin cleanup/new
+git push --set-upstream origin cleanup/new2
